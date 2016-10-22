7f02559c249d4a00d3da99c1b49a149365554b0f
dulte <daniel.heinesen@sf-nett.no>
2016-10-22 15:28:36 +0200

    Add files via upload

--- plot_wavepacket_movie.py
+++ plot_wavepacket_movie.py
@@ -11,31 +11,35 @@ import glob, os  #delete old files automatically
 def animation(func, M, N, xmin, xmax, ymin, ymax, nx,nt):
     x = linspace(xmin, xmax, nx)
     s = zeros_like(x)
-    
+
     old_files = glob.glob("tmp*.png")
     for file in old_files:
         os.remove(file)
-        
+
     plt.ion()
-    #plt.plot(x)
-    
-    lines = plt.plot(x, s)    
+
+
+    lines = plt.plot(x, s)
     plt.axis([xmin,xmax,ymin,ymax])
-    
-    framenumber = 0      
+
+    framenumber = 0
     t_val = linspace(M,N+1,nt)
-    for i in t_val:#range(M, N+1):
+    for i in t_val:
         s = func(x,i)
         lines[0].set_ydata(s)
         plt.draw()
         plt.pause(0.05)
-        #plt.savefig('tmp_%04d.png' %framenumber)
+        plt.savefig('tmp_%04d.png' %framenumber)
         framenumber += 1
-        #time.sleep(1)
-        
-    
+
+
+    os.system("convert -delay 10 -loop 0 tmp*.png wave.gif")
+    old_files = glob.glob("tmp*.png")
+    for file in old_files:
+        os.remove(file)
+
+
 def wave_func(x,t):
     return exp(-(x-3*t)**2)*sin(3*pi*(x-t))
-    
-animation(wave_func,-1,1,-6,6,-1,1,1001,60)
 
+animation(wave_func,-1,1,-6,6,-1,1,1001,60)
